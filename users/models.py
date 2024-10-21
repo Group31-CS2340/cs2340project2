6ec60ec213da4bdb0b4ea1de7937fc6e6d845ad5
kaddington6 <101146239+kaddington6@users.noreply.github.com>
2024-10-21 13:36:07 -0400

    updating models.py
    fixing models (removing unnecessary models as well)

--- users/models.py
+++ users/models.py
@@ -13,9 +13,6 @@ def register_user(username, first_name, last_name, email, password):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    favorite_restaurants = models.TextField(blank=True)
-    dietary_restrictions = models.CharField(max_length=255, blank=True)
-    favorite_cuisine_types = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -28,8 +25,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-
-class Review(models.Model):
-    restaurant_name = models.CharField("restaurant name", max_length=150, blank=True)
-    rating = models.IntegerField("rating", default=0)
-    review = models.TextField("review", blank=True)
