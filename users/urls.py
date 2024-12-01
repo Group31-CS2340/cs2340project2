99529d7c7fa1ca8f90a0053fdc05392e15d1a51b
Jacob <jacobsisson2022@gmail.com>
2024-12-01 00:24:33 -0500

    Code cleanup

--- users/urls.py
+++ users/urls.py
@@ -10,6 +10,4 @@ urlpatterns = [
     path('spotify_data/', views.spotify_data, name='spotify_data'),
     path('spotify_logout/', views.spotify_logout, name='spotify_logout'),  # Add this line
     path('wrap-generate/', views.wrap_generate, name='wrap_generate'),
-
-
 ]
