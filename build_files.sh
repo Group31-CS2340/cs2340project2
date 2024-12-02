e0ff451f358bdac94b615aba29d585f203ec5c80
826anderson <119andersonchen@gmail.com>
2024-12-01 22:24:07 -0500

    build_files change

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,11 @@
 echo "Installing Packages"
 python3 -m pip install -r requirements.txt
 
+echo "Generating Translation Files"
+django-admin makemessages -a  
+
+echo "Compiling Translations"
+django-admin compilemessages  
 
 echo "Migrating DB"
 python3 manage.py makemigrations --noinput
@@ -8,3 +13,4 @@ python3 manage.py migrate --noinput
 
 echo "Collecting static files"
 python3 manage.py collectstatic --noinput
+
