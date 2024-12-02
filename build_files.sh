7e7cf683c311970ae8353e0398555dd602d10586
826anderson <119andersonchen@gmail.com>
2024-12-01 22:29:50 -0500

    build

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,12 @@
 echo "Installing Packages"
 python3 -m pip install -r requirements.txt
 
+echo "Generating Translation Files"
+django-admin makemessages -a  
+
+echo "Compiling Translations"
+django-admin compilemessages  
+
 echo "Migrating DB"
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
